580d2a53ef219a64004bf077856304a17cd961d5
Denis <java.for@mail.ru>
2017-01-18 09:47:37 +0300

    Update bat_install_addin.bat

--- install_excel_addins/bat_install_addin.bat
+++ install_excel_addins/bat_install_addin.bat
@@ -5,9 +5,9 @@
 
 @echo off
 
-@rem !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+@rem !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 @rem КОДИРОВКА ФАЙЛА ДОЛЖНА БЫТЬ В OEM866
-@rem !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+@rem !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 @rem этот BAT файл запускает скрипт, который производит установку указанной в данном файле надстройки
 @rem в этом файле указываются:
@@ -16,5 +16,32 @@
 @rem - название надстройки без расширени. Пример: test_ezhov
 @rem - расширение. Пример: xla
 
-WScript "X:\Категорийные\Автоматизированная система анализа ассортимента\Инструменты АСАА\Минпартия\install_excel_addins.vbs" "X:\Категорийные\Автоматизированная система анализа ассортимента\Инструменты АСАА\Минпартия\" "PROBLEM-MINQUANTITY(20161017)_0.22" "xlam"
-pause
+@rem путь к исполняемому скрипту
+SET PATH_TO_VBS="X:\Категорийные\Автоматизированная система анализа ассортимента\Инструменты АСАА\Минпартия\install_excel_addins.vbs"
+@rem путь к надстройке
+SET PATH_TO_ADDIN="X:\Категорийные\Автоматизированная система анализа ассортимента\Инструменты АСАА\Минпартия\"
+@rem название файла
+SET ADDIN_NAME="PROBLEM-MINQUANTITY(20161017)_0.22"
+@rem расширеним надстройки
+SET ADDIN_EXT="xlam"
+@rem 1 - показывать, 0 - не показывать
+SET IS_SHOW_MSG="1" 
+
+echo Входные параметры:
+echo PATH_TO_ADDIN: %PATH_TO_ADDIN%
+echo PATH_TO_VBS: %PATH_TO_VBS%
+echo ADDIN_NAME: %ADDIN_NAME%
+echo ADDIN_EXT: %ADDIN_EXT%
+echo IS_SHOW_MSG: %IS_SHOW_MSG%
+
+CScript %PATH_TO_VBS% %PATH_TO_ADDIN% %ADDIN_NAME% %ADDIN_EXT% %IS_SHOW_MSG%
+
+echo ErrorLevel: %ErrorLevel%
+
+if ErrorLevel 1 (
+	C:\Windows\system32\cscript.exe %PATH_TO_VBS% %PATH_TO_ADDIN% %ADDIN_NAME% %ADDIN_EXT% %IS_SHOW_MSG%
+)
+
+if ErrorLevel 1 (
+	pause
+)
