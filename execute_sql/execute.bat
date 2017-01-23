de8c0c8287c3fb6d97225f4bde49637c5fb46697
Denis <java.for@mail.ru>
2017-01-23 11:55:05 +0300

    Update execute.bat

--- execute_sql/execute.bat
+++ execute_sql/execute.bat
@@ -3,5 +3,17 @@
 @rem==================
 
 @echo off
-chcp 866 && cscript %~dp0execute_sql.vbs "log_inactive_mp.txt" "teradata->Provider=MSDASQL.1;Persist Security Info=False;Data Source=teradata_auth---mssql->Provider=MSDASQL.1;Persist Security Info=False;Data Source=OTZ-prod1" "teradata->K:\_Departments\Департамент технологий закупок\Отдел технологий закупок_\00. Папки сотрудников\Арефин\Актуальные_коды\Местные поставщики\script\Неактивные МП код.txt---mssql->E:\_own_repository_data\projects_dev\scripts\inactive_mp\execute_load_to_mssql.sql"
-pause
+
+SET PATH_TO_EXECUTE_DIRECTORY=%~dp0
+echo Путь к папке запуска bat файла: %PATH_TO_EXECUTE_DIRECTORY%
+
+SET LOG_FILE="%PATH_TO_EXECUTE_DIRECTORY%log_burachenko.txt"
+echo Лог файл: %LOG_FILE%
+
+SET CONNECTIONS="teradata->DRIVER={Teradata};DBCNAME=teradata;UID=AREFIN_AV;PWD=Feanoris1992Feanoris1992_;USEINTEGRATEDSECURITY=Y; AUTHENTICATION=ldap;charset=UTF16;"
+echo Подключения: %CONNECTIONS%
+
+SET SCRIPTS="teradata->K:\_Departments\Департамент технологий закупок\Отдел технологий закупок_\00. Папки сотрудников\Арефин\Актуальные_коды\Отчет наполенность МК Бураченко\1 часть.txt---teradata->K:\_Departments\Департамент технологий закупок\Отдел технологий закупок_\00. Папки сотрудников\Арефин\Актуальные_коды\Отчет наполенность МК Бураченко\2 часть.txt"
+echo Скрипты: %SCRIPTS%
+
+cscript %~dp0execute_sql.vbs %LOG_FILE% %CONNECTIONS% %SCRIPTS%
