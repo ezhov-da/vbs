42c9e6cf352e7a2a66ba521a18ba65ecff5a7de4
Denis <java.for@mail.ru>
2017-01-23 12:03:02 +0300

    Update execute.bat

--- execute_sql/execute.bat
+++ execute_sql/execute.bat
@@ -1,19 +1,23 @@
 @rem==================
-@rem кодировка в CP866
+@rem Є®¤Ёа®ўЄ  ў CP866
 @rem==================
 
 @echo off
 
+SET NAME_VBS=execute_sql.vbs
+
 SET PATH_TO_EXECUTE_DIRECTORY=%~dp0
 echo Путь к папке запуска bat файла: %PATH_TO_EXECUTE_DIRECTORY%
 
-SET LOG_FILE="%PATH_TO_EXECUTE_DIRECTORY%log_burachenko.txt"
+SET LOG_FILE="%PATH_TO_EXECUTE_DIRECTORY%log_minpart_mp.txt"
 echo Лог файл: %LOG_FILE%
 
-SET CONNECTIONS="teradata->DRIVER={Teradata};DBCNAME=teradata;UID=AREFIN_AV;PWD=Feanoris1992Feanoris1992_;USEINTEGRATEDSECURITY=Y; AUTHENTICATION=ldap;charset=UTF16;"
+SET CONNECTIONS="teradata->Provider=MSDASQL.1;Persist Security Info=False;Data Source=teradata_auth"
 echo Подключения: %CONNECTIONS%
 
-SET SCRIPTS="teradata->K:\_Departments\Департамент технологий закупок\Отдел технологий закупок_\00. Папки сотрудников\Арефин\Актуальные_коды\Отчет наполенность МК Бураченко\1 часть.txt---teradata->K:\_Departments\Департамент технологий закупок\Отдел технологий закупок_\00. Папки сотрудников\Арефин\Актуальные_коды\Отчет наполенность МК Бураченко\2 часть.txt"
+SET SCRIPTS="teradata->E:\_own_repository_data\projects_dev\macros\minpart\SQL_QUERY.sql"
 echo Скрипты: %SCRIPTS%
 
-cscript %~dp0execute_sql.vbs %LOG_FILE% %CONNECTIONS% %SCRIPTS%
+SET EXECUTE_VBS=%PATH_TO_EXECUTE_DIRECTORY%%NAME_VBS%
+
+cscript %EXECUTE_VBS% %LOG_FILE% %CONNECTIONS% %SCRIPTS%
